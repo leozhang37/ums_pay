116695364a2083cff0fa7be45229abfa01b9fb1d
leo <leozhang37@gmail.com>
2019-07-16 13:37:25 +0800

    init

---
+++ lib/ums_pay.ex
@@ -0,0 +1,18 @@
+defmodule UmsPay do
+  @moduledoc """
+  Documentation for UmsPay.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UmsPay.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ums_pay_test.exs
@@ -0,0 +1,8 @@
+defmodule UmsPayTest do
+  use ExUnit.Case
+  doctest UmsPay
+
+  test "greets the world" do
+    assert UmsPay.hello() == :world
+  end
+end
